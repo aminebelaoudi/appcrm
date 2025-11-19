618d083c304da1030adda56907c31abaaf4de149
Amine BELAOUDI <aminebelaoudi.75@gmail.com>
2025-11-20 00:31:11 +0100

    Remove scheduled command for GHL token refresh

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,10 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // Rafraîchir les tokens GHL tous les jours à minuit
-        $schedule->command('ghl:refresh-tokens')
-                 ->dailyAt('00:00')
-                 ->timezone('America/Montreal');
+        // $schedule->command('inspire')->hourly();
     }
 
     /**
