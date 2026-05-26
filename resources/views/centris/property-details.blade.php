1806e12822706ff9b0dea886aec16bd8ccc51aa6
amine <aminebelaoudi.75@gmail.com>
2026-05-26 14:07:39 +0100

    Fix Centris submission contact link

--- resources/views/centris/property-details.blade.php
+++ resources/views/centris/property-details.blade.php
@@ -1430,7 +1430,7 @@
                                 <td>{{ $submission['phone'] ?? '-' }}</td>
                                 <td class="action-menu">
                                     @if(!empty($submission['externalContactId']))
-                                        <button class="action-button submission-view-button" onclick="viewCentrisSubmissionContact(event, @json($submission['externalContactId']))" title="Voir le contact">
+                                        <button class="action-button submission-view-button" data-contact-id="{{ $submission['externalContactId'] }}" onclick="viewCentrisSubmissionContact(event)" title="Voir le contact">
                                             <svg viewBox="0 0 24 24" aria-hidden="true">
                                                 <path d="M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17M12,4.5C7,4.5 2.73,7.61 1,12C2.73,16.39 7,19.5 12,19.5C17,19.5 21.27,16.39 23,12C21.27,7.61 17,4.5 12,4.5Z"/>
                                             </svg>
@@ -2391,8 +2391,10 @@
             }
         }
 
-        function viewCentrisSubmissionContact(event, contactId) {
+        function viewCentrisSubmissionContact(event) {
             event.stopPropagation();
+
+            const contactId = event.currentTarget?.dataset?.contactId;
 
             if (!contactId) {
                 alert('ID du contact introuvable');
